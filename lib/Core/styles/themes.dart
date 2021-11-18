b24c15ef15a3f22b015486789e10373020e060fa
hossambakry <https://github.com/Hossam-Bakry>
2021-11-18 12:43:12 +0200

    create project

--- lib/Core/styles/themes.dart
+++ lib/Core/styles/themes.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+class MyTheme {
+  static const primaryColor = Color.fromRGBO(57, 165, 82, 1.0);
+
+  static ThemeData lightThem = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: primaryColor,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  );
+}
